0ca90226446912c7bcf397e9d93a4dc2f1c98458
navicyclical <navicyclical@users.noreply.github.com>
2016-04-08 13:46:33 -0700

    Update cookiemonster.js

--- cookiemonster.js
+++ cookiemonster.js
@@ -8,13 +8,13 @@ var cookieMonster = { "name" : "Fred",
 cookieMonster.eat = function(food){
   var score = 0;
   for (var i = 0; i < cookieMonster.favorite_foods.length; i++){
-    if (food === cookieMonster.favorite_foods) {
+    if (food === cookieMonster.favorite_foods[i]) {
       score += 2;
     } // loop over good foods and add the result to score
     for (var j = 0; j < cookieMonster.good_foods.length; j++){
       if(food === cookieMonster.good_foods[j]){
         score ++;
-      } for (var k = 0; k < cookieMonster.bad_foods.length; k++){
+         } for (var k = 0; k < cookieMonster.bad_foods.length; k++){
         if (food === cookieMonster.bad_foods[k]){
           score --;
         } for (var l = 0; l < cookieMonster.hated_foods.length; l++){
@@ -23,7 +23,7 @@ cookieMonster.eat = function(food){
           }
         }
       }
-	  }
+    }
   }
   return score;
 };
